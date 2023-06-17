c9d7d59538dc61f8ee2c72e7b7a60980899625a5
Gaurav Pal <gauravpal95106@gmail.com>
2023-06-17 20:36:25 +0530

    added hero section switching next prev and other tweaks

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -7,24 +7,26 @@ const Header = () => {
 
     return (
         <>
-            <div  className="flex flex-row h-28 justify-between px-4 md:px-12 items-center  text-white absolute top-0 z-10 w-full">
-                <div><h1 className="font-bold">Movie Box</h1></div>
-                <div className=" text-xs gap-3 hidden md:flex">
+            <div  className="flex flex-row h-28 justify-between px-4 md:px-12 items-center  text-white  top-0 z-10 w-full fixed">
+                <div><h1 className="cursor-pointer font-bold">Movie Box</h1></div>
+                <div className=" text-xs gap-14 hidden md:flex">
                     <div>Movies</div>
                     <div>TV Shows</div>
                     <div className="">Favourites</div> 
                 </div>
-                <div className=" rounded-md  flex"><input className="w-20 bg-[#c5c5c56c] rounded-md"/><img className="bg-[#ffffffa2]  rounded-md mx-1" width="25" height="25" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1"/> </div>
+                <div className=" rounded-md  flex"><input className="w-20 bg-[#c5c5c56c] rounded-md"/><img className="bg-[#ffffffa2]  rounded-md mx-1 " width="25" height="25" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1"/> </div>
                 <div  className="space-y-2 cursor-pointer md:hidden relative" onClick={toggleNav} >
-                    <span className="block w-5 h-0.5 bg-white"></span>
-                    <span className="block w-8 h-0.5 bg-white"></span>
-                    <span className="block w-8 h-0.5 bg-white"></span>
-                    <div id="nav" onClick={disableNav} onMouseLeave={disableNav}  className="w-auto right-0 top-10   absolute bg-slate-300 ">
-                        <ul className={active ? "" : "hidden"}>
-                            <li className="m-3 ho">Movies</li>
-                            <li className="m-3 ho">TV shows</li>
-                            <li className="m-3 ho">Favourites</li>
-                        </ul>
+                    <span className="block w-5 h-1 bg-white hover:bg-red-500"></span>
+                    <span className="block w-8 h-1 bg-white hover:bg-red-500"></span>
+                    <span className="block w-8 h-1 bg-white hover:bg-red-500"></span>
+                    <div id="nav" onClick={disableNav} onMouseLeave={disableNav}  className="w-auto  right-0 top-10   absolute rounded-md bg-black  border-[1px] border-white  ">
+                        <div className={active ? "" : "hidden"}>
+                            <ul className="p-10 bg-[#c5c5c536]">
+                                <li className="m-3 text-lg hover:text-red-500">Movies</li>
+                                <li className="m-3 text-lg hover:text-red-500">TV shows</li>
+                                <li className="m-3 text-lg hover:text-red-500">Favourites</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/component/hero/hero.jsx
+++ src/component/hero/hero.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import {  useState } from "react"
+import {  useState, useEffect } from "react"
 import { useSelector } from 'react-redux'
 
 const Hero = (props) => {
     const [current, setCurrent] = useState(0)
-    // eslint-disable-next-line react/prop-types
     const details = props.data.results
     const baseUrl = useSelector(state => state.home.url.backdrop)
-    console.log(details);
-    React.useEffect(() => {
+    const nextHero = () => setCurrent((current + 1) % details.length)
+    const prevHero = () => {
+        if(current===0)
+            setCurrent(details.length-1)
+        else
+            setCurrent((current - 1) % details.length)
+    }
+
+    useEffect(() => {
         const next = (current + 1) % details.length;
         const id = setTimeout(() => setCurrent(next), 100000);
         return () => clearTimeout(id);
@@ -20,7 +26,7 @@ const Hero = (props) => {
             <div style={{backgroundImage: 'url(' + baseUrl + details[current].backdrop_path + ')' }} className= {`absolute h-full w-full  brightness-50  bg-cover bg-center -z-20  `} ></div>
             <div className="h-[80vh] w-full flex justify-center items-center pt-16 z-20">
                 <div className=" text-white flex flex-col gap-8">
-                    <h1 className="pl-4 tracking-widest uppercase text-6xl sm:text-8xl md:text-9xl text-white z-20">{details[current].original_title} </h1>
+                    <h1 className="pl-4 tracking-widest uppercase text-5xl sm:text-7xl md:text-8xl text-white ">{details[current].original_title} </h1>
                     <p className=' pl-4 text-xs md:text-sm w-80 md:w-96 text-[rgba(255,255,255,0.5)]'>{details[current].overview}</p>
                     <div className="flex gap-3">
                         <div className="text-sm p-1 px-5 text-[rgba(255,255,255,0.5)]  border-[rgba(255,255,255,0.4)] border-lg rounded-full border-[1px]"><p>{details[current].adult ? "Adult: True" : "Adult: False"}</p></div>
@@ -29,15 +35,18 @@ const Hero = (props) => {
                     </div>
                 </div>
             </div>
-            <div className=" flex justify-between ">
+            <div className=" flex justify-between p-4 ">
                 <div className="p-5 flex gap-3">
-                    <div className="h-8 w-14 border-[1px] border-white rounded-lg text-center"><p className="text-xl text-white">&larr;</p></div>
-                    <div className="h-8 w-14 border-[1px] border-white rounded-lg text-center"><p className="text-xl text-white">&rarr;</p></div>
+                    <div onClick={prevHero} className=" cursor-pointer h-8 w-14 border-[1px] hover:border-red-500 border-white rounded-lg text-center"><p className="text-xl hover:text-red-500 text-white">&larr;</p></div>
+                    <div onClick={nextHero} className="cursor-pointer h-8 w-14 border-[1px] border-white hover:border-red-500 rounded-lg text-center"><p className="text-xl text-white hover:text-red-500">&rarr;</p></div>
                 </div>
                 
                 <div className="flex items-center p-3 h-auto md:p-7  border-[1px] border-white rounded-lg">
                     <h1 className='text-2xl text-white'>{details[current].vote_average} /10</h1>
                 </div>
+            </div>
+            <div className='h-14 bg-gradient-to-b from-[rgba(0,0,0)] to-grey-300'>
+
             </div>
         </div>
     )
