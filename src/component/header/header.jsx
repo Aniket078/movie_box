a172c7d6f44ea6d6473f5894af08abdba5f28010
Gaurav Pal <gauravpal95106@gmail.com>
2023-06-17 13:38:27 +0530

    created homepage

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -3,16 +3,14 @@ const Header = () => {
 
     return (
         <>
-            <div className="flex flex-row h-10">
-                {/* logo */}
-                <div>Movie box</div>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+            <div className="flex flex-row h-28 justify-between items-center px-12 text-white absolute top-0 z-10 w-full">
+                <div><h1 className="font-bold">Movie Box</h1></div>
+                <div className="flex text-xs gap-3">
+                    <div>Movies</div>
+                    <div>TV Shows</div>
+                    <div className="">Favourites</div> 
                 </div>
-                <div></div>
+                <div className="w-10 rounded-md bg-[#c5c5c56c] flex"><input className=" w-full bg-[#c5c5c56c] rounded-md"/><img className="bg-[#c5c5c56c] " width="25" height="25" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1"/> </div>
             </div>
         </>
     )
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,8 +3,30 @@ const Home = () => {
 
     return (
         <>
-            <div className="h-screen w-full bg-cover bg-[url('https://images.unsplash.com/photo-1683009427660-b38dea9e8488?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60')] ">
+            <div className="relative h-screen w-full ">
+                <div className="absolute h-full w-full  brightness-50  bg-cover -z-20  bg-[url('https://images.unsplash.com/photo-1683009427660-b38dea9e8488?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60')]"></div>
+                <div className="h-[80vh] w-full flex justify-center items-center pt-16 z-20">
+                    <div className="max-w-;g text-white flex flex-col gap-8">
+                        <h1 className="tracking-widest uppercase text-6xl sm:text-8xl md:text-9xl text-white z-20">Top gun maverick </h1>
+                        <div className="flex gap-3">
+                            <div className="text-sm p-1 px-5 text-[rgba(255,255,255,0.5)]  border-[rgba(255,255,255,0.4)] border-lg rounded-full border-[1px]"><p1>by piyushh chddi</p1></div>
+                            <div className="text-sm p-1 px-5 text-[rgba(255,255,255,0.5)] border-[rgba(255,255,255,0.4)] border-lg rounded-full border-[1px]"><p1>2022</p1></div>
+                            <div className="text-sm p-1 px-5 text-[rgba(255,255,255,0.5)] border-[rgba(255,255,255,0.4)] border-lg rounded-full border-[1px]"><p1>1h20m</p1></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="h-[20vh] flex justify-between ">
+                    <div className="p-5 flex gap-3">
+                        <div className="h-8 w-14 border-[1px] border-white rounded-lg text-center"><p className="text-xl text-white">&larr;</p></div>
+                        <div className="h-8 w-14 border-[1px] border-white rounded-lg text-center"><p className="text-xl text-white">&rarr;</p></div>
+                    </div>
+                    <div className="h-16 w-32 border-[1px] border-white rounded-lg">
 
+                    </div>
+                    <div className="h-16 w-32 border-[1px] border-white rounded-lg">
+
+                    </div>
+                </div>
             </div>
         </>
     )
